aa0c9cd22329df783650143097e7b6a10b81110f
VINXIS <oykxf2@gmail.com>
2019-03-13 15:24:12 -0600

    fix bugs

--- handlers/osuHandle.go
+++ handlers/osuHandle.go
@@ -9,13 +9,15 @@ import (
 
 // OsuHandle handles commands that are regarding osu!
 func OsuHandle(s *discordgo.Session, m *discordgo.MessageCreate, args []string, osuAPI *osuapi.Client, playerCache []structs.PlayerData, mapCache []structs.MapData) {
-	mainArg := args[1]
-	switch mainArg {
-	case "link":
-		go osucommands.Link(s, m, args, osuAPI, playerCache)
-	case "recent", "r", "rs":
-		go osucommands.Recent(s, m, args, osuAPI, playerCache, "recent", mapCache)
-	case "recentb", "rb", "recentbest":
-		go osucommands.Recent(s, m, args, osuAPI, playerCache, "best", mapCache)
+	if len(args) > 1 {
+		mainArg := args[1]
+		switch mainArg {
+		case "link":
+			go osucommands.Link(s, m, args, osuAPI, playerCache)
+		case "recent", "r", "rs":
+			go osucommands.Recent(s, m, args, osuAPI, playerCache, "recent", mapCache)
+		case "recentb", "rb", "recentbest":
+			go osucommands.Recent(s, m, args, osuAPI, playerCache, "best", mapCache)
+		}
 	}
 }
