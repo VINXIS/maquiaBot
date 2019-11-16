a591a2e44c655fba5ca7ee648584c366ed3c4f76
VINXIS <oykxf2@gmail.com>
2019-11-15 23:16:47 -0700

    move clean to 1 file

--- handlers/general-commands/clean.go
+++ handlers/general-commands/clean.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 
+	osuapi "../../osu-api"
 	structs "../../structs"
 	tools "../../tools"
 	"github.com/bwmarrin/discordgo"
@@ -33,4 +34,42 @@ func Clean(s *discordgo.Session, m *discordgo.MessageCreate, cache []structs.Pla
 	err = ioutil.WriteFile("./data/osuData/profileCache.json", jsonCache, 0644)
 	tools.ErrRead(err)
 	s.ChannelMessageSend(m.ChannelID, "Cleaned player cache!")
+}
+
+// CleanFarm cleans the all farmerdog ratings
+func CleanFarm(s *discordgo.Session, m *discordgo.MessageCreate, cache []structs.PlayerData, osuAPI *osuapi.Client) {
+	if m.Author.ID != "92502458588205056" {
+		s.ChannelMessageSend(m.ChannelID, "YOU ARE NOT VINXIS.........")
+		return
+	}
+
+	// Farm Data
+	farmData := structs.FarmData{}
+	f, err := ioutil.ReadFile("./data/osuData/mapFarm.json")
+	tools.ErrRead(err)
+	_ = json.Unmarshal(f, &farmData)
+
+	// Update
+	for i := range cache {
+		if cache[i].Osu.Username != "" && cache[i].Farm.Rating == 0.00 {
+			cache[i].Osu = osuapi.User{}
+		}
+		cache[i].FarmCalc(osuAPI, farmData)
+	}
+
+	// Save
+	jsonCache, err := json.Marshal(cache)
+	if err != nil {
+		s.ChannelMessageSend(m.ChannelID, "Error with wiping data!")
+		tools.ErrRead(err)
+		return
+	}
+	err = ioutil.WriteFile("./data/osuData/profileCache.json", jsonCache, 0644)
+	if err != nil {
+		s.ChannelMessageSend(m.ChannelID, "Error with wiping data!")
+		tools.ErrRead(err)
+		return
+	}
+
+	s.ChannelMessageSend(m.ChannelID, "Updated farmerdog ratings!")
 }
