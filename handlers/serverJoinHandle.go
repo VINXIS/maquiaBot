36265fc871b94d562d4898ca2d39fc5e8ae5905c
VINXIS <oykxf2@gmail.com>
2019-11-24 20:13:27 -0700

    note about bot status when joining server

--- handlers/serverJoinHandle.go
+++ handlers/serverJoinHandle.go
@@ -8,7 +8,7 @@ import (
 func ServerJoin(s *discordgo.Session, g *discordgo.GuildCreate) {
 	channels := g.Channels
 	for _, channel := range channels {
-		_, err := s.ChannelMessageSend(channel.ID, "Hello! My default prefix is `$` but you can change it by using `$prefix` or `maquiaprefix`")
+		_, err := s.ChannelMessageSend(channel.ID, "Hello! My default prefix is `$` but you can change it by using `$prefix` or `maquiaprefix`\nPlease remember that this bot is still currently under development so this bot may constantly go on and off as more features are being added!")
 		if err == nil {
 			return
 		}
