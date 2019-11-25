4fcb0e670e95892ced0062bf4857336af2b97654
VINXIS <oykxf2@gmail.com>
2019-11-25 01:23:09 -0700

    Fuckin hell

--- tools/adminCheck.go
+++ tools/adminCheck.go
@@ -7,8 +7,8 @@ func AdminCheck(s *discordgo.Session, m *discordgo.MessageCreate, server discord
 	if m.Author.ID != server.OwnerID {
 		member, _ := s.GuildMember(server.ID, m.Author.ID)
 		for _, roleID := range member.Roles {
-			role, _ := s.State.Role(m.GuildID, roleID)
-			if role.Permissions&discordgo.PermissionAdministrator != 0 || role.Permissions&discordgo.PermissionManageServer != 0 {
+			role, err := s.State.Role(m.GuildID, roleID)
+			if err == nil && (role.Permissions&discordgo.PermissionAdministrator != 0 || role.Permissions&discordgo.PermissionManageServer != 0) {
 				admin = true
 				break
 			}
