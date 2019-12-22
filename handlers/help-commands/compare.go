a4a1c07f271160bf5d6e26bfa6bf20caee7538d7
VINXIS <oykxf2@gmail.com>
2019-12-22 12:23:30 -0700

    update help

--- handlers/help-commands/compare.go
+++ handlers/help-commands/compare.go
@@ -7,7 +7,7 @@ import (
 // Compare explains the compare functionality
 func Compare(embed *discordgo.MessageEmbed) *discordgo.MessageEmbed {
 	embed.Author.Name = "Command: c / compare"
-	embed.Description = "`[osu] (c|compare) [link] <osu! username> [-m <mod> [-nostrict]]` lets you show your score(s) on a map."
+	embed.Description = "`[osu] (c|compare) [link] <osu! username> [-m <mod> [-nostrict]|-all] ` lets you show your score(s) on a map."
 	embed.Fields = []*discordgo.MessageEmbedField{
 		&discordgo.MessageEmbedField{
 			Name:   "[link]",
@@ -27,6 +27,11 @@ func Compare(embed *discordgo.MessageEmbed) *discordgo.MessageEmbed {
 		&discordgo.MessageEmbedField{
 			Name:   "[-nostrict]",
 			Value:  "If the score should have that mod combination exactly, or if it can have other mods included.",
+			Inline: true,
+		},
+		&discordgo.MessageEmbedField{
+			Name:   "[-all]",
+			Value:  "Show all scores the user has made on the map.",
 			Inline: true,
 		},
 	}
