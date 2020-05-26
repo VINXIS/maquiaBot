e8099ad823e9e5ccb8e12124f50e68451d975393
VINXIS <oykxf2@gmail.com>
2020-05-26 13:21:54 -0600

    run caps function on prev message

--- handlers/general-commands/capitalization.go
+++ handlers/general-commands/capitalization.go
@@ -12,6 +12,20 @@ import (
 func Capitalization(s *discordgo.Session, m *discordgo.MessageCreate, capsType string) {
 	message := strings.Join(strings.Split(m.Content, " ")[1:], " ")
 
+	if len(message) == 0 {
+		msgs, err := s.ChannelMessages(m.ChannelID, -1, m.ID, "", "")
+		if err != nil {
+			s.ChannelMessageSend(m.ChannelID, "Ur really funny mate")
+			return
+		}
+		for _, msg := range msgs {
+			if msg.Author.ID != m.Author.ID {
+				message = msgs[0].Content
+				break
+			}
+		}
+	}
+
 	switch capsType {
 	case "allLower":
 		message = strings.ToLower(message)
