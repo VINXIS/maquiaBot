14327c708fedb884161419826022a76b2cc9c6ad
VINXIS <oykxf2@gmail.com>
2020-06-18 17:42:05 -0600

    more verbose toggle result

--- handlers/admin-commands/toggle.go
+++ handlers/admin-commands/toggle.go
@@ -3,6 +3,7 @@ package admincommands
 import (
 	"encoding/json"
 	"io/ioutil"
+	"strconv"
 	"strings"
 	"time"
 
@@ -30,6 +31,8 @@ func Toggle(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// Set new information in server data
 	serverData.Time = time.Now()
 	flagged := false
+	target := ""
+	status := "false"
 	if strings.Contains(m.Content, "-a") || strings.Contains(m.Content, "-announce") {
 		if serverData.AnnounceChannel == m.ChannelID {
 			serverData.AnnounceChannel = ""
@@ -37,22 +40,32 @@ func Toggle(s *discordgo.Session, m *discordgo.MessageCreate) {
 			serverData.AnnounceChannel = m.ChannelID
 		}
 		flagged = true
+		target = "The announcement channel"
+		status = "this channel"
 	}
 	if strings.Contains(m.Content, "-d") || strings.Contains(m.Content, "-daily") {
 		serverData.Daily = !serverData.Daily
 		flagged = true
+		target = "`daily`"
+		status = strconv.FormatBool(serverData.Daily)
 	}
 	if strings.Contains(m.Content, "-os") || strings.Contains(m.Content, "-osu") {
 		serverData.OsuToggle = !serverData.OsuToggle
 		flagged = true
+		target = "`osuToggle`"
+		status = strconv.FormatBool(serverData.OsuToggle)
 	}
 	if strings.Contains(m.Content, "-s") || strings.Contains(m.Content, "-stats") {
 		serverData.AllowAnyoneStats = !serverData.AllowAnyoneStats
 		flagged = true
+		target = "`AllowAnyoneStats`"
+		status = strconv.FormatBool(serverData.AllowAnyoneStats)
 	}
 	if strings.Contains(m.Content, "-v") || strings.Contains(m.Content, "-vibe") {
 		serverData.Vibe = !serverData.Vibe
 		flagged = true
+		target = "`Vibe`"
+		status = strconv.FormatBool(serverData.Vibe)
 	}
 	if !flagged {
 		s.ChannelMessageSend(m.ChannelID, "No flags given! Please use one of the flags listed in `help toggle`!")
@@ -65,5 +78,6 @@ func Toggle(s *discordgo.Session, m *discordgo.MessageCreate) {
 	err = ioutil.WriteFile("./data/serverData/"+m.GuildID+".json", jsonCache, 0644)
 	tools.ErrRead(s, err)
 
+	s.ChannelMessageSend(m.ChannelID, target+" has now been set to "+status+". Obtaining server info...")
 	go gencommands.ServerInfo(s, m)
 }
