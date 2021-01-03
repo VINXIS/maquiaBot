c4b8bd611c2788a28d5dbf8c45c3adcdcbf04b95
VINXIS <oykxf2@gmail.com>
2021-01-03 02:02:04 +0000

    guildcreate / serverjoin is fired at the beginning of starting the bot, add extra security measures

--- handlers/serverJoinHandle.go
+++ handlers/serverJoinHandle.go
@@ -3,10 +3,12 @@ package handlers
 import (
 	"encoding/json"
 	"io/ioutil"
+	"log"
 	"math"
 	"strconv"
 	"time"
 
+	"maquiaBot/config"
 	tools "maquiaBot/tools"
 
 	"github.com/bwmarrin/discordgo"
@@ -16,8 +18,29 @@ import (
 func ServerJoin(s *discordgo.Session, g *discordgo.GuildCreate) {
 	s.UpdateStatus(0, strconv.Itoa(len(s.State.Guilds))+" servers")
 
+	// Check for a guild ID
+	if g.ID == "" {
+		dm, err := s.UserChannelCreate(config.Conf.BotHoster.UserID)
+		if err == nil {
+			s.ChannelMessageSend(dm.ID, "An error occured in obtaining information from a server join.")
+		}
+		log.Println("An error occured in obtaining information from a server join Guild info below:")
+		log.Println(g)
+		return
+	}
+
 	// Obtain server data
-	server, _ := s.Guild(g.ID)
+	server, err := s.Guild(g.ID)
+	if err != nil {
+		dm, err := s.UserChannelCreate(config.Conf.BotHoster.UserID)
+		if err == nil {
+			s.ChannelMessageSend(dm.ID, "An error occured in obtaining information from a server join.")
+			return
+		}
+		log.Println("An error occured in obtaining information from a server join Guild info below:")
+		log.Println(g)
+		return
+	}
 	serverData := tools.GetServer(*server, s)
 
 	// Check if bot was already in server or if server is unavailable
@@ -26,7 +49,7 @@ func ServerJoin(s *discordgo.Session, g *discordgo.GuildCreate) {
 		return
 	}
 
-	_, err := s.ChannelMessageSend(g.ID, "Hello! My default prefix is `$` but you can change it by using `$prefix` or `maquiaprefix`\nPlease remember that this bot is still currently under development so this bot may constantly go on and off as more features are being added!")
+	_, err = s.ChannelMessageSend(g.ID, "Hello! My default prefix is `$` but you can change it by using `$prefix` or `maquiaprefix`\nPlease remember that this bot is still currently under development so this bot may constantly go on and off as more features are being added!")
 	if err != nil {
 		for _, channel := range g.Channels {
 			serverData.AnnounceChannel = channel.ID
