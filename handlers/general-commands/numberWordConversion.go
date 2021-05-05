fcf2c9badc386b7e31fd16d58569c8f4eee579d8
VINXIS <oykxf2@gmail.com>
2021-05-05 01:25:32 -0600

    add limits

--- handlers/general-commands/numberWordConversion.go
+++ handlers/general-commands/numberWordConversion.go
@@ -13,14 +13,14 @@ func Ntow(s *discordgo.Session, m *discordgo.MessageCreate) {
 	parts := strings.Split(m.Content, " ")[1:]
 	converted := ""
 	for _, part := range parts {
-		if num, err := strconv.ParseFloat(part, 64); err == nil {
+		if num, err := strconv.ParseFloat(part, 64); err == nil && num < 99999999999999999999999999999999999999999999999999999999999999999 {
 			converted = tools.Ntow(num)
 			break
 		}
 	}
 
 	if converted == "" {
-		s.ChannelMessageSend(m.ChannelID, "No number found! Reminder that the largest number allowed is `9223372036854775807`")
+		s.ChannelMessageSend(m.ChannelID, "No number found! Reminder that the largest number allowed is (technically) `99999999999999999999999999999999999999999999999999999999999999999`")
 	} else {
 		s.ChannelMessageSend(m.ChannelID, converted)
 	}
