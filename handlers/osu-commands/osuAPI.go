857105eefdc8fa9d5eb040c757862f36e044e8bd
VINXIS <oykxf2@gmail.com>
2022-03-30 19:23:52 -0600

    hee hee hoo hoo

--- handlers/osu-commands/osuAPI.go
+++ handlers/osu-commands/osuAPI.go
@@ -2,5 +2,5 @@ package osucommands
 
 import osuapi "maquiaBot/osu-api"
 
-// OsuAPI is the api client used for osu! stuff
+// OsuAPI is the api client used for osu! queries
 var OsuAPI *osuapi.Client
